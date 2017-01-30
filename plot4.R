6323c6eb54da8904a5f4f54bcc1a043a00d41a6f
alimojiz1 <ali.mojiz1@gmail.com>
2017-01-30 22:30:07 +0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,8 @@
+data<-read.table("E:/R_workspace/household_power_consumption.txt",sep=";",header = TRUE,na.strings = "?")
+data$Date<-as.Date(data$Date)
+datatouse<-data[data$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(datatouse$Date, datatouse$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+datatouse<-cbind(SetTime,datatouse)
 par(mfrow=c(2,2))
 plot(datatouse$SetTime, datatouse$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
 plot(datatouse$SetTime, datatouse$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
